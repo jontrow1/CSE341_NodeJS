9e387da47a395861ec201541cea329075905e230
Jon Trowbridge <tro19008@byui.edu>
2021-05-08 07:17:29 -0400

    one more time

---
+++ shopProjectHeroku/app.js
@@ -0,0 +1,27 @@
+const PORT = process.env.PORT || 3000
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'CSE341_NodeJS/readingProject/views'); 
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+//app.listen(3000);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
